383e23f43c3e1d19c5213e7b85c9f2bf34b5f3b4
Viktor <viktormoden@yahoo.se>
2026-04-22 21:51:56 +0200

    Log in and Create Account page

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,13 @@
+import "./CreateAccount.css"
+
+export default function CreateAccount() {
+    return (
+        <div>
+            <h1>Log in</h1>
+            <form>
+                <input type="text" placeholder="Name"/>
+                <input type="text" placeholder="Password"/>                
+            </form>
+        </div>
+    )
+}
