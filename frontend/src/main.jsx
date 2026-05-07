f9aa20f77c009372fa0aca7ee4ef435a7b5295eb
AntonThörnwall <anton.thornwall123@gmail.com>
2026-05-07 15:51:09 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './pages/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
