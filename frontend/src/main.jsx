2e73b2eca2e6d04797f67aa27d33734a60845b2d
Viktor <viktormoden@yahoo.se>
2026-05-07 11:54:08 +0200

    Fixed routing bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,8 @@ import Home from './pages/Home.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>      
-      <Home />
+      {/* <Home /> */}
+      <App></App>
     </BrowserRouter>    
   </StrictMode>,
 )
